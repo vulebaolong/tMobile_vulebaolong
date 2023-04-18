e407e5e643ce2587c5c80fd0bbd452944335ec55
vulebaolong <vulebaolong@gmail.com>
2023-04-18 15:01:23 +0700

    fix tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./admin/**/*.{html,js}"],
+    darkMode: "class",
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
