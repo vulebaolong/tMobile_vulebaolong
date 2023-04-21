7711c7e8772622e00f54ab4b2dc23a270171452d
Vu Le Bao Long <vulebaolong@gmail.com>
2023-04-21 20:13:30 +0700

    customer display

---
+++ customer/controller/main.js
@@ -0,0 +1,20 @@
+const $ = (selector) => document.querySelector(selector);
+const $$ = (selector) => document.querySelectorAll(selector);
+
+//click savechange
+$("#opencart").addEventListener("click", function (params) {
+    $(".cart-section").classList.toggle("hidden");
+
+    setTimeout(() => {
+        $(".cart-backdrop").classList.toggle("opacity-0");
+        $(".cart-slide").classList.toggle("translate-x-full");
+    }, 0);
+});
+
+$("#closecart").addEventListener("click", function (params) {
+    $(".cart-backdrop").classList.toggle("opacity-0");
+    $(".cart-slide").classList.toggle("translate-x-full");
+    setTimeout(() => {
+        $(".cart-section").classList.toggle("hidden");
+    }, 500);
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./admin/**/*.{html,js}"],
+    content: ["./**/*.{html,js}"],
     darkMode: "class",
     theme: {
         extend: {
