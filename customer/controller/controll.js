7681e7265223c987d783a5ca83466df7b17bda69
vulebaolong <vulebaolong@gmail.com>
2023-04-28 02:33:20 +0700

    add count

---
+++ customer/controller/controll.js
@@ -0,0 +1,179 @@
+const $ = (selector) => document.querySelector(selector);
+const $$ = (selector) => document.querySelectorAll(selector);
+const BASE_URL = "https://643a58bdbd3623f1b9b164ba.mockapi.io/customor/";
+
+function createItem(value) {
+    return axios.post(BASE_URL, value);
+}
+
+function readItem(id = null, url = BASE_URL) {
+    if (id !== null) {
+        url += id;
+    }
+    return axios.get(url);
+}
+
+function updateItem(item) {
+    return axios.put(BASE_URL + item.id, item);
+}
+
+function deleteItem(id) {
+    return axios.delete(BASE_URL + id);
+}
+
+function render(arrData) {
+    const productListEl = $(".product_list");
+    let string = "";
+    arrData.reverse().forEach((el) => {
+        string += `<div class="product_item group">
+                         <!-- PRODUCT ITEM TITLE -->
+                        <div class="transition duration-300 group-hover:opacity-40">
+                            <!-- IMAGE -->
+                            <div
+                                class="min-h-80 bg-white aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md lg:aspect-none lg:h-44 xl:h-64"
+                            >
+                                <img
+                                    src="${el.img}"
+                                    alt="img"
+                                    class="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                                />
+                            </div>
+                            <!-- TITLE -->
+                            <div class="mt-4 space-y-2">
+                                <p
+                                    class="text-base font-bold text-gray-900 truncate text-center"
+                                >
+                                    ${el.name}
+                                </p>
+                                <p
+                                    class="text-sm font-medium text-gray-700 text-center truncate"
+                                >
+                                    ${el.price} ₫
+                                </p>
+                                <div>
+                                    <span class="inline-block mt-4 p-1 bg-neutral-200 rounded text-sm">
+                                        ${el.type}
+                                    </span>
+                                </div>
+                                <div class="">
+                                    <span
+                                        class="text-sm font-bold text-gray-900 truncate text-center"
+                                    >
+                                        Mô tả:
+                                    </span>
+                                    <span class="mt-1 w-full text-sm text-gray-500">
+                                    ${el.desc}
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- OVERLAY -->
+                        <div
+                            class="group/product_overlay absolute flex flex-col justify-center z-50 inset-0 p-6 space-y-2 bg-black bg-opacity-70 group-hover:opacity-100 opacity-0 transition duration-300"
+                        >
+                            <div
+                                class="mb-5 transition duration-500 -translate-y-full opacity-0 group-hover/product_overlay:translate-y-0 group-hover/product_overlay:opacity-100"
+                            >
+                                <div class="">
+                                    <span
+                                        class="text-sm font-bold text-white truncate text-center"
+                                    >
+                                        Screen:
+                                    </span>
+                                    <span class="mt-1 w-full text-sm text-white">
+                                    ${el.screen}
+                                    </span>
+                                </div>
+                                <div class="">
+                                    <span
+                                        class="text-sm font-bold text-white truncate text-center"
+                                    >
+                                        Camera sau:
+                                    </span>
+                                    <span class="mt-1 w-full text-sm text-white">
+                                    ${el.backCamera}
+                                    </span>
+                                </div>
+                                <div class="">
+                                    <span
+                                        class="text-sm font-bold text-white truncate text-center"
+                                    >
+                                        Camera trước:
+                                    </span>
+                                    <span class="mt-1 w-full text-sm text-white">
+                                    ${el.frontCamera}
+                                    </span>
+                                </div>
+                            </div>
+                            <button
+                               onclick="openCart()"
+                                class="w-full btn btn-blue transition duration-500 translate-y-full opacity-0 group-hover/product_overlay:translate-y-0 group-hover/product_overlay:opacity-100"
+                            >
+                                Add To Cart
+                            </button>
+                        </div>
+                    </div>`;
+    });
+    productListEl.innerHTML = string;
+}
+
+function init() {
+    //danh sách
+    readItem(null, "https://643a58bdbd3623f1b9b164ba.mockapi.io/admin/")
+        .then((result) => {
+            render(result.data);
+        })
+        .catch((err) => {});
+
+    // giỏ hàng
+    readItem()
+        .then((result) => {
+            let quantity = result.data.length;
+            if (quantity > 0) addCount(quantity);
+
+            // render(result.data);
+        })
+        .catch((err) => {});
+}
+
+function openComponent(elSection, elContent, elBackdrop) {
+    $(elSection).classList.toggle("hidden");
+
+    setTimeout(() => {
+        $(elBackdrop).classList.toggle("opacity-0");
+        $(elContent).classList.toggle("translate-x-full");
+    }, 0);
+}
+
+function closeComponent(elSection, elContent, elBackdrop) {
+    $(elBackdrop).classList.toggle("opacity-0");
+    $(elContent).classList.toggle("translate-x-full");
+    setTimeout(() => {
+        $(elSection).classList.toggle("hidden");
+    }, 400);
+}
+
+function debounce(fn, ms) {
+    let timer;
+
+    return function () {
+        // Nhận các đối số
+        const args = arguments;
+        const context = this;
+
+        if (timer) clearTimeout(timer);
+
+        timer = setTimeout(() => {
+            fn.apply(context, args);
+        }, ms);
+    };
+}
+
+function addCount(count) {
+    $(".cart_count ").classList.remove("hidden");
+    $(".cart_count ").classList.add("bum");
+    $(".cart_count ").innerText = count;
+    setTimeout(() => {
+        $(".cart_count ").classList.remove("bum");
+    }, 300);
+}
