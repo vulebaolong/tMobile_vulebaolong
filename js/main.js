c67b7fe9467209c70bc4c88e79b8a122ac66ae6c
Vu Le Bao Long <vulebaolong@gmail.com>
2023-05-03 00:54:01 +0700

    fix folder

--- js/main.js
+++ js/main.js
@@ -4,3 +4,9 @@ document.querySelector("#theme-toggle").addEventListener("click", function () {
     document.querySelector("#theme-toggle-light-icon").classList.toggle("hidden");
     document.querySelector("html").classList.toggle("dark");
 });
+$("#M_btn_show-nav").addEventListener("click", function () {
+    openComponent("#M-nav", "#M-nav_content", "#M-nav_backdrop");
+});
+$("#M_btn_close-nav").addEventListener("click", function () {
+    closeComponent("#M-nav", "#M-nav_content", "#M-nav_backdrop");
+});
